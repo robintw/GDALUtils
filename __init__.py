c9ede7e599de8f705e072e6c09a8808c7ef4763d
Robin Wilson <robin@rtwilson.com>
2014-04-04 15:10:12 +0100

    Added image_like function, to produce a new image with the same configuration as a given image.

--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
 from run_fn_on_image_bands import apply_fn_to_image
 from image_to_df import image_to_df
+from image_like import image_like
---
+++ image_like.py
@@ -0,0 +1,51 @@
+import gdal, gdalconst
+import numpy as np
+import pandas as pd
+
+def image_like(orig_im, output_filename, drv=None, datatype=None, nbands=None, xdim=None, ydim=None):
+	# Can work when orig_im is a filename or a GDAL image
+	if type(orig_im) == str:
+		orig_im = gdal.Open(orig_im)
+		need_to_close = True
+	else:
+		need_to_close = False
+
+	# Can work when drv is a GDAL Driver or a string with a driver name
+	# So, if a string, convert to a GDAL Driver
+	if type(drv) == str:
+		drv = gdal.GetDriverByName(drv)
+	if type(drv) == gdal.Dataset:
+		drv = drv.GetDriver()
+
+	b = orig_im.GetRasterBand(1)
+
+	if datatype is None:
+		datatype = b.datatype
+	else:
+		if datatype == float:
+			datatype = gdalconst.GDT_Float32
+		elif datatype == int:
+			datatype = gdalconst.GDT_Int32
+		elif datatype == bool:
+			datatype = gdalconst.GDT_Byte
+
+	if nbands is None:
+		nbands = orig_im.RasterCount
+
+	if xdim is None:
+		xdim = orig_im.RasterXSize
+
+	if ydim is None:
+		ydim = orig_im.RasterYSize
+
+	new_im = drv.Create(output_filename, xdim, ydim, nbands, datatype)
+
+	new_im.SetProjection(orig_im.GetProjection())
+	new_im.SetGeoTransform(orig_im.GetGeoTransform())
+
+	if need_to_close:
+		del orig_im
+
+	del b
+
+	return new_im
