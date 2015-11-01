a0a9f847c10061cf575a1b4bc89ac421c3d49f5f
Robin Wilson <robin@rtwilson.com>
2015-11-01 17:36:52 +0000

    Improved to be able to be used from code, returning all stats

--- compare_images.py
+++ compare_images.py
@@ -13,7 +13,7 @@ def singleband_image_to_array(filename):
     return arr
 
 
-def compare(filename1, filename2):
+def compare_images(filename1, filename2):
     a1 = singleband_image_to_array(filename1)
     a2 = singleband_image_to_array(filename2)
 
@@ -21,27 +21,34 @@ def compare(filename1, filename2):
 
     diff = diff[np.isfinite(diff)]
 
+    minval = diff.min()
+    maxval = diff.max()
+    meanval = diff.mean()
+    medval = np.median(diff)
+
     print("Image comparison:\n")
     print("Comparing:")
     print("\t %s" % filename1)
     print("\t %s" % filename2)
     print("")
-    print("Min: %.10f" % diff.min())
-    print("Mean: %.10f" % diff.mean())
-    print("Median: %.10f" % np.median(diff))
-    print("Max: %.10f" % diff.max())
+    print("Min: %.10f" % minval)
+    print("Mean: %.10f" % meanval)
+    print("Median: %.10f" % medval)
+    print("Max: %.10f" % maxval)
 
-    val = ((np.abs(diff) < 0.1).sum() / float(np.size(diff))) * 100.0
-    print("perc diff < 0.01 %f" % val)
+    percval = ((np.abs(diff) < 0.01).sum() / float(np.size(diff))) * 100.0
+    print("perc diff < 0.01 %f" % percval)
 
     # if np.allclose(a1, a2):
     #     print("All elements close according to np.allclose")
     # else:
     #     print("All elements NOT CLOSE according to np.allclose")
 
+    return minval, meanval, medval, maxval, percval
+
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print('USAGE: compare_images.py IMAGE1 IMAGE2')
         sys.exit(1)
 
-    compare(sys.argv[1], sys.argv[2])
+    compare_images(sys.argv[1], sys.argv[2])
